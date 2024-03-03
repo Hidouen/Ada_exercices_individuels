5105a251e9fb51efd39122db1ebf092d3bc43b36
Hidouen <hidouen.dev@gmail.com>
2024-03-03 19:01:37 +0100

    ex02 - step 2 done

--- 02_guess_a_number/my_answer/js/script.js
+++ 02_guess_a_number/my_answer/js/script.js
@@ -2,8 +2,33 @@ function askNumber(){
     let givenNumber = 'a';
     while (isNaN(givenNumber)){
         givenNumber = parseInt(prompt("Please enter a number"));
+        if (isNaN(givenNumber)){
+            alert("That's not a number");
+        }
     }
-    console.log(givenNumber);
+    return givenNumber;
+}
+function didIWin(givenNumber, goodNumber){
+    if (givenNumber < goodNumber){
+        alert("Plus grand");
+        return false;
+    }
+    else if (givenNumber > goodNumber){
+        alert("Plus petit");
+        return false;
+    }
+    else {
+        alert("Bravo ! Vous avez deviné le nombre");
+        return true;
+    }
 }
 
-askNumber();
+function gamePlay(){
+    let givenNumber;
+    let goodNumber = 22;
+    do {
+        givenNumber = askNumber();
+    } while (didIWin(givenNumber, goodNumber) === false)
+}
+
+gamePlay();
