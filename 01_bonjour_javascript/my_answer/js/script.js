547e2d5aad0887c6edb51b0cd11823b32c59292f
Hidouen <hidouen.dev@gmail.com>
2024-03-03 18:08:46 +0100

    step 3

--- 01_bonjour_javascript/my_answer/js/script.js
+++ 01_bonjour_javascript/my_answer/js/script.js
@@ -1,8 +1,18 @@
-let userName = prompt("What's your name ?");
-let message1 = '👋 Bonjour ' + userName;
-document.body.innerHTML += '<h2 id="welcome"></h2>';
-document.getElementById("welcome").innerText = message1;
-let userAge = 2023 - parseInt(prompt("What's your year of birth ?"));
-let message2 = "Vous avez " + userAge + "ans";
-document.body.innerHTML += '<h3 id="age"></h3>';
-document.getElementById("age").innerText = message2;
+function askName(){
+    let userName = prompt("What's your name ?");
+    let message = "👋 Bonjour " + userName;
+    document.body.innerHTML += '<h2 id="welcome"></h2>';
+    document.getElementById("welcome").innerText = message;
+}
+function askBirthYear(){
+    let userYear = 0;
+    while (userYear == 0 || userYear < 1850 || userYear > 2023){
+        userYear = parseInt(prompt("And what is your year of birth ?"));
+    }
+    let userAge = 2024 - userYear;
+    message = "Wow ! Vous avez " + userAge + "ans, ça ne se voit pas 😉"
+    document.body.innerHTML += '<h3 id="age"></h3>';
+    document.getElementById("age").innerText = message;
+}
+askName();
+askBirthYear();
