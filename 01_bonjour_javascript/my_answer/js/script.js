857abf64c6eae094a4f5de65f5daaedf20690c6e
Hidouen <hidouen.dev@gmail.com>
2024-03-03 18:00:06 +0100

    step 2

--- 01_bonjour_javascript/my_answer/js/script.js
+++ 01_bonjour_javascript/my_answer/js/script.js
@@ -1,4 +1,8 @@
-let username = prompt("What's your name ?");
-let message = '👋 Bonjour ' + username;
+let userName = prompt("What's your name ?");
+let message1 = '👋 Bonjour ' + userName;
 document.body.innerHTML += '<h2 id="welcome"></h2>';
-document.getElementById("welcome").innerText = message;
+document.getElementById("welcome").innerText = message1;
+let userAge = 2023 - parseInt(prompt("What's your year of birth ?"));
+let message2 = "Vous avez " + userAge + "ans";
+document.body.innerHTML += '<h3 id="age"></h3>';
+document.getElementById("age").innerText = message2;
--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 My individual exersises as part of my certification at Ada Tech School.
 
 In each directory you can find :
+
     -The subject of the exersise in the "name_exersise.md" file
+
     -My answer at this exersise in the "my_answer" directory
