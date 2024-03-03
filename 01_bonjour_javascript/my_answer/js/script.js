5063a5f52375bc4993676495205c8de7b864cd57
Hidouen <hidouen.dev@gmail.com>
2024-03-03 18:21:23 +0100

    ex01 done

--- 01_bonjour_javascript/my_answer/js/script.js
+++ 01_bonjour_javascript/my_answer/js/script.js
@@ -1,15 +1,22 @@
 function askName(){
-    let userName = prompt("What's your name ?");
+    let userName = prompt("Please enter your name :");
     let message = "👋 Bonjour " + userName;
     document.body.innerHTML += '<h2 id="welcome"></h2>';
     document.getElementById("welcome").innerText = message;
 }
 function askBirthYear(){
     let userYear = 0;
+    let userMonth = 0;
     while (userYear == 0 || userYear < 1850 || userYear > 2023){
-        userYear = parseInt(prompt("And what is your year of birth ?"));
+        userYear = parseInt(prompt("Enter your birth year ?"));
+    }
+    while (userMonth < 1 || userMonth > 12){
+        userMonth = parseInt(prompt("Last question : what's your birth month ? (between 1 and 12)"));
     }
     let userAge = 2024 - userYear;
+    if (userMonth > 2){
+        userAge--;
+    }
     message = "Wow ! Vous avez " + userAge + "ans, ça ne se voit pas 😉"
     document.body.innerHTML += '<h3 id="age"></h3>';
     document.getElementById("age").innerText = message;
