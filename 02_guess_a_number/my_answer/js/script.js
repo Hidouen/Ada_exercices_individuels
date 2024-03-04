c2eedda892a66b0ca9ed3ac193cdf30012439cab
Hidouen <hidouen.dev@gmail.com>
2024-03-04 15:24:27 +0100

    ex02_step 5 done

--- 02_guess_a_number/my_answer/js/script.js
+++ 02_guess_a_number/my_answer/js/script.js
@@ -1,47 +1,59 @@
-function askNumber(){
-    let numberP2;
-    do {
-        numberP2 = parseInt(prompt("Player 2 : Try to guess the player 1 number"));
-        if (isNaN(numberP2)){
-            alert("It's not a number");
-        }
-    } while (isNaN(numberP2))
-    return numberP2;
-}
-
-function didIWin(givenNumber, toGuessNumber){
-    if (givenNumber < toGuessNumber){
-        alert("bigger");
-        return false;
-    }
-    else if (givenNumber > toGuessNumber){
-        alert("smaller");
-        return false;
-    }
-    else{
-        alert("Congrats' ! You guessed the number");
-        return true;
-    }
-}
 
 function askNumberToGuess(){
     let toGuessNumber = 51;
     while (toGuessNumber < 0 || toGuessNumber > 50 || isNaN(toGuessNumber)){
         toGuessNumber = parseInt(prompt("Player 1 : Please enter a number between 0 and 50", "0 < ? < 50"));
-        if (toGuessNumber < 0 || toGuessNumber > 50 || isNaN(toGuessNumber)){
-            alert("it's not a number or the number is outside of the range (between 0 to 50)");
+        if (toGuessNumber < 0 || toGuessNumber > 50){
+            alert("The number is outside of the range (between 0 to 50)");
+        }
+        else if (isNaN(toGuessNumber)){
+            alert("It's not a number");
         }
     }
     return(toGuessNumber);
 }
 
+// function askNumber(){
+//     let numberP2;
+//     do {
+//         numberP2 = parseInt(prompt("Player 2 : Try to guess the player 1 number"));
+//         if (isNaN(numberP2)){
+//             alert("It's not a number");
+//         }
+//     } while (isNaN(numberP2))
+//     return numberP2;
+// }
+
+function didIWin(givenNumber, toGuessNumber){
+    if (givenNumber < toGuessNumber){
+        return false;
+    }
+    else if (givenNumber > toGuessNumber){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
 function gamePlay(){
     const toGuessNumber = askNumberToGuess();
     let givenNumber = 0;
-    do {
-        givenNumber = askNumber();
-    } while (didIWin(givenNumber, toGuessNumber) === false)
+    let attempts = 0;
+    let result = false;
 
+    document.getElementById("GuessTheNumber").addEventListener("submit", function(event) {
+        event.preventDefault(); // Pour empêcher le rechargement de la page
+        givenNumber = parseInt(document.getElementById("givenNumber").value);
+        result = didIWin(givenNumber, toGuessNumber);
+        if (result === false){
+            attempts++;
+            document.getElementById("attempts").innerHTML = "False. Attempts = " + attempts;
+        }
+        else{
+            document.getElementById("attempts").innerHTML = "TRUE ! Congratulations !";
+        }
+      });
 }
 
 gamePlay();
