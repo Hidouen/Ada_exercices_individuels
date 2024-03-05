af81da2fbc78105f671ee04c4a000131abe76728
Hidouen <hidouen.dev@gmail.com>
2024-03-05 19:00:58 +0100

    ex04_step2 done

--- 04_parlez_vous_le_morse/my_answer/js/script.js
+++ 04_parlez_vous_le_morse/my_answer/js/script.js
@@ -1,3 +1,36 @@
+const latinToMorse = {
+	'A':'.-',
+	'B':'-...',
+	'C':'-.-.',
+	'D':'-..',
+	'E':'.',
+	'F':'..-.',
+	'G':'--.',
+	'H':'....',
+	'I':'..',
+	'J':'.---',
+	'K':'-.-',
+	'L':'.-..',
+	'M':'--',
+	'N':'-.',
+	'O':'---',
+	'P':'.--.',
+	'Q':'--.-',
+	'R':'.-.',
+	'S':'...',
+	'T':'-',
+	'U':'..-',
+	'V':'...-',
+	'W':'.--',
+	'X':'-..-',
+	'Y':'-.--',
+	'Z':'--..'
+}
+
+function translateLatinCharacter(letter){
+    return latinToMorse[letter];
+}
+
 function getLatinCharacterList(str){
     let tab = [];
     for (i = 0; i < str.length; i++){
@@ -6,4 +39,12 @@ function getLatinCharacterList(str){
     return tab;
 }
 
-console.log(getLatinCharacterList("Hello, world"));
+function displayResult(letter){
+    letter = letter.toUpperCase();
+    console.log(letter);
+    let message = "'" + letter + "' character in morse code is '" + translateLatinCharacter(letter) + "'";
+    document.body.innerHTML += '<h3 id="answer"></h3>';
+    document.getElementById("answer").innerText = message;
+}
+
+displayResult('A');
