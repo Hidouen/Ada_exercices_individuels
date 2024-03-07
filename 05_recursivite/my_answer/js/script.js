587aaf1f0ce1a55e193befae1ce87a6ef074445d
Hidouen <hidouen.dev@gmail.com>
2024-03-07 23:57:35 +0100

    ex05 improving functions security

--- 05_recursivite/my_answer/js/script.js
+++ 05_recursivite/my_answer/js/script.js
@@ -6,20 +6,17 @@ function sum1(tab) {
     return result;
 }
 
-function sum2(tab, i) {
-    let result = 0;
-    if (i < tab.length) {
-        result = tab[i] + sum2(tab, i + 1);
+function sum2(tab) {
+    if (tab.length === 0) {
+        return 0;
     }
     else {
-        return result;
+        return tab[0] + sum2(tab.slice(1));
     }
-
-    return result;
 }
 
 function factorial(n) {
-    if (n < 0) {
+    if (n < 0 || isNaN(n)) {
         return 0;
     }
     else if (n === 0) {
@@ -31,20 +28,16 @@ function factorial(n) {
 }
 
 function fibonacci(x) {
-	if (x < 0) {
-		return (-1);
+    if (x <= 1 || isNaN(x)) {
+        return x;
     }
-	if (x == 0) {
-		return (0);
+    else {
+        return fibonacci(x - 1) + fibonacci(x - 2);
     }
-	if (x == 1) {
-		return (1);
-    }
-	return (fibonacci(x - 1) + fibonacci(x - 2));
 }
 
 const nbrs_tab = [1, 2, 3];
 console.log("step 1 - iterative sum\nResult with [1, 2, 3] array = " + sum1(nbrs_tab))
-console.log("step 2 - recursive sum\nResult with [1, 2, 3] array = " + sum2(nbrs_tab, 0));
+console.log("step 2 - recursive sum\nResult with [1, 2, 3] array = " + sum2(nbrs_tab));
 console.log("step 3 - recursive factorial\nResult (with n=3) = " + factorial(3));
 console.log("step 4 - fibonacci recursive\nresult (with x=5) = " + fibonacci(7));
